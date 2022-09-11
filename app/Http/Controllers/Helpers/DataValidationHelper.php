978f39ddc541a1f60f9dd33ee0164232362a307a
Kevin del Rosario <delrosariokevinm@gmail.com>
2022-09-11 14:19:24 +0800

    Migrated Existing Model Functions and Created Helper Class

--- app/Http/Controllers/Helpers/DataValidationHelper.php
+++ app/Http/Controllers/Helpers/DataValidationHelper.php
@@ -7,5 +7,40 @@ use Illuminate\Http\Request;
 
 class DataValidationHelper extends Controller
 {
-    //
+    public static function apidie($string, $code = 200){
+        $string .= "";
+        http_response_code($code);
+        if(defined('SCRIPT') && SCRIPT)
+            throw new Exception($string);
+        die($string);
+    }
+
+    public static function missing_request($field){
+        return (!isset($_REQUEST[$field]) or !$_REQUEST[$field]);
+    }
+
+    public static function missing_post($field){
+        return (!isset($_POST[$field]) or !$_POST[$field]);
+    }
+
+    public static function missing_get($field){
+        return (!isset($_GET[$field]) or !$_GET[$field]);
+    }
+
+    public static function is_json($string) {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
+    
+    public function contains($haystack, $needle, $case_sensitive = true){
+        if(!$case_sensitive)
+            return (strpos(strtolower($haystack), strtolower($needle)) !== FALSE);
+        return (strpos($haystack, $needle) !== FALSE);
+    }
+
+    public function startswith($haystack, $needle, $case_sensitive = true){
+        if(!$case_sensitive)
+            return (strpos(strtolower($haystack), strtolower($needle)) === 0);
+        return (strpos($haystack, $needle) === 0);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,62 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public const SUCCESS_MESSAGE = "success";
+    public const FAILED_MESSAGE = "failed";
+    public const SYSTEM_ERROR_MESSAGE = "Something Went Wrong! Please Contact Developer.";
+
+    public function append_user_comments($id, $comments){
+        try{
+            $user = User::select(['comments'])->where('id',$id)->first();
+            if(!empty($user))
+            {
+                $user->comments .= "\n".$comments;
+                User::where('id',$id)->update([
+                    "comments" => $user->comments
+                ]);
+            }else{
+                $message = "No Such User (" . $id . ")";
+
+                return response()->json([
+                    'message' => $message,
+                    'message_status' => self::FAILED_MESSAGE
+                ], 404);
+            }
+        }catch(\Exception $e){
+            return response()->json([
+                'message' => self::SYSTEM_ERROR_MESSAGE,
+                'message_status' => self::FAILED_MESSAGE
+            ], 500);
+        }
+    }
+
+    public function get_user_by_id($id){
+        try{
+            $result = User::select(['id','name','comments'])->where('id',$id)->first();
+            if(!empty($result))
+            {
+                $message = "Success Fully Retrived Record!";
+
+                return response()->json([
+                    'message' => $message,
+                    'message_status' => self::SUCCESS_MESSAGE,
+                    'data' => $result
+                ], 200);
+            }else{
+                $message = "No Such User (" . $id . ")";
+
+                return response()->json([
+                    'message' => $message,
+                    'message_status' => self::FAILED_MESSAGE
+                ], 404);
+            }
+        }catch(\Exception $e){
+            
+            return response()->json([
+                'message' => self::SYSTEM_ERROR_MESSAGE,
+                'message_status' => self::FAILED_MESSAGE
+            ], 500);
+        }
+    }
 }
